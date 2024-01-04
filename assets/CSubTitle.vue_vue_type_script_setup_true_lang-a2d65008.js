419afbe958a7ac98561e2456b348f337c7ab76fb
xiaohuohumax <xiaohuohumax@users.noreply.github.com>
2024-01-04 05:56:49 +0000

    deploy: 2aabd905edeb63c7b4a61398192b270fcba7fe7d

---
+++ assets/CSubTitle.vue_vue_type_script_setup_true_lang-a2d65008.js
@@ -0,0 +1,1 @@
+import{d as a,a6 as t,a9 as s,a8 as o,ad as r,ae as n,af as c}from"./index-e356217c.js";import{N as d}from"./Card-5655ee7b.js";const i={class:"font-bold text-lg"},m=a({__name:"CSubTitle",props:{context:{}},setup(l){return(e,p)=>(t(),s(c(d),{size:"small",bordered:!1},{default:o(()=>[r("div",i,n(e.context),1)]),_:1}))}});export{m as _};
