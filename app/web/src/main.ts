01f068a02285a420a0ae22c22c6debe0c5bd0f28
xiaohuohumax <1915970986@qq.com>
2024-10-18 16:29:12 +0800

    feat: :sparkles: markdown代码块高亮替换为Shiki

--- app/web/src/main.ts
+++ app/web/src/main.ts
@@ -1,15 +1,36 @@
+import Shiki from '@shikijs/markdown-it'
 import { logger } from '@xiaohuohumax/miaoji-util'
+import { full as emoji } from 'markdown-it-emoji'
+import MarkdownItGitHubAlerts from 'markdown-it-github-alerts'
+import { config } from 'md-editor-v3'
 import { createPinia } from 'pinia'
 import persistedstate from 'pinia-plugin-persistedstate'
 import { createApp } from 'vue'
+import { useAppStore } from '~/store/app'
 import appConfig from './app.config'
 import App from './App.vue'
 import i18n from './i18n'
 import router from './router'
-import { useAppStore } from './store/app'
 import title from './util/title'
 import 'tailwindcss/lib/css/preflight.css'
 import './style/index.css'
+
+async function setMarkdownConfig() {
+  const MarkdownItShiki = await Shiki({
+    themes: {
+      light: 'one-light',
+      dark: 'one-dark-pro',
+    },
+  })
+
+  config({
+    markdownItConfig: (md) => {
+      md.use(emoji)
+      md.use(MarkdownItGitHubAlerts)
+      md.use(MarkdownItShiki)
+    },
+  })
+}
 
 async function run() {
   logger.setDefaultLevel(import.meta.env.VITE_LOGGER_LEVEL)
@@ -28,8 +49,10 @@ async function run() {
   const appStore = useAppStore()
   appStore.initLoad()
 
+  setMarkdownConfig()
+
   app.use(router)
   app.mount('#app')
 }
 
-run().catch()
+run().catch(logger.error)
--- app/web/src/views/components/CMarkdown.vue
+++ app/web/src/views/components/CMarkdown.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import { full as emoji } from 'markdown-it-emoji'
-import MarkdownItGitHubAlerts from 'markdown-it-github-alerts'
-import { config, MdPreview } from 'md-editor-v3'
+import { MdPreview } from 'md-editor-v3'
 import { useAppStore } from '~/store/app'
 import 'md-editor-v3/lib/style.css'
 import 'markdown-it-github-alerts/styles/github-colors-light.css'
@@ -15,19 +13,12 @@ export interface CMarkdownProps {
 
 defineProps<CMarkdownProps>()
 const appStore = useAppStore()
-
-config({
-  markdownItConfig: (md) => {
-    md.use(emoji)
-    md.use(MarkdownItGitHubAlerts)
-  },
-})
 </script>
 
 <template>
   <MdPreview
     v-if="content" :model-value="content" :editor-id="`editor-${id}`" :theme="appStore.theme.name"
-    :code-foldable="true" code-theme="github" :language="appStore.language"
+    :code-foldable="true" :no-highlight="true" :show-code-row-number="false" preview-theme="github" :language="appStore.language"
   />
 </template>
 
@@ -60,5 +51,23 @@ config({
 
 .md-mask {
   background: linear-gradient(to bottom, transparent 20%, var(--n-color-modal));
+}
+
+::v-deep(.md-editor-preview .md-editor-code pre) {
+  background-color: transparent !important;
+}
+
+::v-deep(.md-editor code),
+::v-deep(.md-editor-code code),
+::v-deep(.md-editor-code .md-editor-code-head) {
+  transition: background-color 0.3s cubic-bezier(0.4, 0, 0.2, 1);
+}
+
+::v-deep(.md-editor-preview h2) {
+  border-bottom: none !important;
+}
+
+::v-deep(.default-theme .md-editor-code pre code) {
+  box-shadow: none !important;
 }
 </style>
